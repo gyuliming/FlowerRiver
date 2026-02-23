40e7d52759053fc147011096c35fc42a3dfd35db
GYULIM <gyuliming@gmail.com>
2026-02-24 04:42:27 +0900

    chore: 스프링부트 프로젝트 초기 생성 및 패키지 구조 세팅

---
+++ src/main/java/com/wms/flowerwms/FlowerwmsApplication.java
@@ -0,0 +1,13 @@
+package com.wms.flowerwms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerwmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowerwmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wms/flowerwms/FlowerwmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wms.flowerwms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerwmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
