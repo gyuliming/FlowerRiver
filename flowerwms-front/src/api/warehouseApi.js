2b1a61ce2f473eecc2f8b408c9016409ea381521
GYULIM <gyuliming@gmail.com>
2026-03-01 06:44:36 +0900

    feat: 창고 상세 페이지 구현(창고 정보/구역 현황/카카오맵)

--- flowerwms-front/src/api/warehouseApi.js
+++ flowerwms-front/src/api/warehouseApi.js
@@ -1,11 +1,13 @@
 import http from './http.js'
 
+// 창고 목록
 export function fetchWarehouses({ keyword, code, name, address, page, size }) {
     return http.get('/warehouses', {
         params: { keyword, code, name, address, page, size }
     })
 }
 
+// 창고 상세
 export function fetchWarehouseDetail(id) {
     return http.get(`/warehouses/${id}`)
 }
