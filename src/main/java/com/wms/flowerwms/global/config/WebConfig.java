eafa81c3be42c3785edfcd55fe7079fc81b16fa5
GYULIM <gyuliming@gmail.com>
2026-03-08 19:12:08 +0900

    fix: 매핑 주소 고정 IP도 할당되게 변경

--- src/main/java/com/wms/flowerwms/global/config/WebConfig.java
+++ src/main/java/com/wms/flowerwms/global/config/WebConfig.java
@@ -9,7 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173",
+                        "http://localhost",
+                        "http://localhost:80",
+                        "http://3.38.66.111") // 탄력적 주소 (고정 IP)
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
